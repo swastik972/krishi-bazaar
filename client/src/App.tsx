6b376f0b96f03c9d1c02ac5399857f92f6be0d1b
rajanpanth <rajan.pantha@samriddhicollege.edu.np>
2025-02-11 14:20:26 +0545

    router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,4 @@
-// import { Switch, Route } from "wouter";
-// import { QueryClientProvider } from "@tanstack/react-query";
-// import { queryClient } from "./lib/queryClient";
-// import { Toaster } from "@/components/ui/toaster";
-// import Header from "@/components/layout/Header";
-// import Footer from "@/components/layout/Footer";
-// import Home from "@/pages/Home";
-// import Marketplace from "@/pages/Marketplace";
-// import Partners from "@/pages/Partners";
-// import Contact from "@/pages/Contact";
-// import NotFound from "@/pages/not-found";
-
-// function Router() {
-//   return (
-//     <div className="min-h-screen bg-[#FAF3E0]">
-//       <Header />
-//       <main>
-//         <Switch>
-//           <Route path="/" component={Home} />
-//           <Route path="/marketplace" component={Marketplace} />
-//           <Route path="/partners" component={Partners} />
-//           <Route path="/contact" component={Contact} />
-//           <Route component={NotFound} />
-//         </Switch>
-//       </main>
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// function App() {
-//   return (
-//     <QueryClientProvider client={queryClient}>
-//       <Router />
-//       <Toaster />
-//     </QueryClientProvider>
-//   );
-// }
-
-// export default App;
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Switch, Route } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
@@ -50,30 +10,28 @@ import Partners from "@/pages/Partners";
 import Contact from "@/pages/Contact";
 import NotFound from "@/pages/not-found";
 
-function AppRouter() {
+function Router() {
   return (
-    <Router>
-      <div className="min-h-screen bg-[#FAF3E0]">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/marketplace" element={<Marketplace />} />
-            <Route path="/partners" element={<Partners />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="min-h-screen bg-[#FAF3E0]">
+      <Header />
+      <main>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/marketplace" component={Marketplace} />
+          <Route path="/partners" component={Partners} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <AppRouter />
+      <Router />
       <Toaster />
     </QueryClientProvider>
   );
